b871360115cb7858b0ffd2984ae708372a17150a
Kagami <shimura.kyohei7617@gmail.com>
2024-04-06 21:56:58 +0900

    feat: item_sub crud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,217 @@
 "use client";
 import { useState } from "react";
+import { Item } from "../../types";
 
 export default function Home() {
   const [response, setResponse] = useState<any>(null);
+  const [id, setId] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
-  type MessageProps = {
-    id: number;
-    name: string;
+  // GET
+  const getMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const item: Item = {
+      name,
+    };
+    let url: string;
+
+    id
+      ? (url = `http://localhost:8000/item_subs/${id}`)
+      : (url = `http://localhost:8000/item_subs/`);
+
+    try {
+      const res = await fetch(url, { cache: "no-store" });
+      if (!res.ok) {
+        throw new Error("Error");
+      }
+      const items = await res.json();
+      setResponse(items);
+      setMessage("Item Got!");
+      return items;
+    } catch (error: unknown) {
+      console.log(error);
+    }
   };
-  const message: MessageProps = {
-    id: 1,
-    name: "kagami",
+
+  // POST
+  const postMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const item: Item = {
+      name,
+    };
+
+    try {
+      const res = await fetch("http://localhost:8000/item_subs/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(item),
+      });
+      if (res.ok) {
+        const jsonData = await res.json();
+        setResponse(jsonData);
+        setMessage("Item Created!");
+      } else {
+        throw new Error("Error");
+      }
+    } catch (error: unknown) {
+      console.log(error);
+    }
   };
 
-  type ItemProps = {
-    name: string;
+  // PUT
+  const putMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const item: Item = {
+      name,
+    };
+
+    try {
+      const res = await fetch(`http://localhost:8000/item_subs/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(item),
+      });
+      if (res.ok) {
+        const jsonData = await res.json();
+        setResponse(jsonData);
+        setMessage("Item Updated!");
+      } else {
+        throw new Error("Error");
+      }
+    } catch (error: unknown) {
+      console.log(error);
+    }
   };
-  const item: ItemProps = {
-    name: "PC",
-  };
 
-  const sendMessage = async (item: ItemProps) => {
-    const res = await fetch("http://localhost:8000/item_subs/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(item),
-    });
+  // DELETE
+  const deleteMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    if (res.ok) {
-      // レスポンスのJSONデータを取得
-      const jsonData = await res.json();
+    const item: Item = {
+      name,
+    };
 
-      // jsonDataを画面に表示
-      setResponse(jsonData);
-    } else {
-      alert("エラーが発生しました");
+    try {
+      const res = await fetch(`http://localhost:8000/item_subs/${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        const jsonData = await res.json();
+        setResponse(jsonData);
+        setMessage("Item Deleted!");
+      } else {
+        throw new Error("Error");
+      }
+    } catch (error: unknown) {
+      console.log(error);
     }
   };
 
   return (
     <div className='bg-slate-600 h-screen flex flex-col items-center justify-center'>
       <h1 className='text-3xl font-bold'>FastAPI Ping</h1>
-      <form>
-        <button
-          type='button'
-          className='bg-blue-300 hover:bg-blue-700 text-white p-3 rounded-md mt-4'
-          onClick={() => sendMessage(item)}
+
+      <div className='flex justify-center'>
+        {/* GET */}
+        <form
+          className='flex flex-col justify-center m-4'
+          onSubmit={getMessage}
         >
-          送信
-        </button>
-      </form>
+          <input
+            type='number'
+            className='shadow border rounded w-full py-2 px-4 m-4 text-gray-700 leading-tight focus:outline-none'
+            placeholder='Enter item_sub Id'
+            onChange={(e) => setId(parseInt(e.target.value))}
+          />
+          <button
+            type='submit'
+            className='w-full bg-blue-300 hover:bg-blue-700 text-white py-2 px-4 m-4 rounded-md'
+          >
+            GET
+          </button>
+        </form>
+
+        {/* POST */}
+        <form
+          className='flex flex-col justify-center m-4'
+          onSubmit={postMessage}
+        >
+          <input
+            type='text'
+            className='shadow border rounded w-full py-2 px-4 m-4 text-gray-700 leading-tight focus:outline-none'
+            placeholder='Enter item_sub Name'
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button
+            type='submit'
+            className='w-full bg-blue-300 hover:bg-blue-700 text-white py-2 px-4 m-4 rounded-md'
+          >
+            POST
+          </button>
+        </form>
+      </div>
+
+      <div className='flex justify-center'>
+        {/* PUT */}
+        <form className='flex flex-col justify-center' onSubmit={putMessage}>
+          <input
+            type='number'
+            className='shadow border rounded w-full py-2 px-4 m-4 text-gray-700 leading-tight focus:outline-none'
+            placeholder='Enter item_sub Id'
+            onChange={(e) => setId(parseInt(e.target.value))}
+          />
+          <input
+            type='text'
+            className='shadow border rounded w-full py-2 px-4 m-4 text-gray-700 leading-tight focus:outline-none'
+            placeholder='Enter item_sub Name'
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button
+            type='submit'
+            className='w-full bg-blue-300 hover:bg-blue-700 text-white py-2 px-4 m-4 rounded-md'
+          >
+            PUT
+          </button>
+        </form>
+
+        {/* DELETE */}
+        <form
+          className='flex flex-col justify-center m-4'
+          onSubmit={deleteMessage}
+        >
+          {/* 位置調整 */}
+          <input
+            type='hidden'
+            className='shadow border rounded w-full py-2 px-4 m-4 text-gray-700 leading-tight focus:outline-none'
+          />
+          <input
+            type='number'
+            className='shadow border rounded w-full py-2 px-4 m-4 text-gray-700 leading-tight focus:outline-none'
+            placeholder='Enter item_sub Id'
+            onChange={(e) => setId(parseInt(e.target.value))}
+          />
+          <button
+            type='submit'
+            className='w-full bg-blue-300 hover:bg-blue-700 text-white py-2 px-4 m-4 rounded-md'
+          >
+            DELETE
+          </button>
+        </form>
+      </div>
+
       {response && (
         <div className='mt-4 bg-green-100 p-4 rounded-md'>
-          <p className='text-green-500 font-medium'>サーバーからの応答:</p>
+          <p className='text-green-500 font-medium'>
+            サーバーからの応答：{message}
+          </p>
           <pre>{JSON.stringify(response, null, 2)}</pre>
         </div>
       )}
